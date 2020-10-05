0f2696aee2079b6376030fe02e12383bfb2f631e
Marc Gomersbach <marc.gomersbach@hva.nl>
2020-10-05 15:59:25 +0200

    Up to step 4, added the product_add_dialog and the methods necessary to build it. As well as the remove/delete on swipe.

--- app/src/main/java/com/example/madlevel4task1/MainActivity.kt
+++ app/src/main/java/com/example/madlevel4task1/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.madlevel4task1
 
+import android.annotation.SuppressLint
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -31,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         fab_add.setOnClickListener {
-            onAddProduct()
+            showAddProductDialog()
         }
     }
 
@@ -41,7 +47,7 @@ class MainActivity : AppCompatActivity() {
         rv_shoppinglist.adapter = shoppingListAdapter
         rv_shoppinglist.addItemDecoration(DividerItemDecoration(applicationContext, DividerItemDecoration.VERTICAL))
 
-        //createItemTouchHelper
+        createItemTouchHelper()
 
         rv_shoppinglist.apply {
             setHasFixedSize(true)
@@ -65,8 +71,74 @@ class MainActivity : AppCompatActivity() {
         // Delete the entire list!
     }
 
-    private fun onAddProduct() {
-        // Open popup pls
+    @SuppressLint("InflateParams")
+    private fun showAddProductDialog() {
+        val builder = AlertDialog.Builder(applicationContext)
+        builder.setTitle(getString(R.string.add_product_dialog_title))
+        val dialogLayout = layoutInflater.inflate(R.layout.add_product_dialog, null)
+        val productName = dialogLayout.findViewById<EditText>(R.id.txt_product_name)
+        val amount  = dialogLayout.findViewById<EditText>(R.id.txt_amount)
+
+        builder.setView(dialogLayout)
+        builder.setPositiveButton(R.string.dialog_ok_btn) {
+            _:DialogInterface, _: Int ->
+            addProduct(productName, amount)
+        }
+        builder.show()
     }
 
+    private fun addProduct(txtProductName: EditText, txtAmount: EditText) {
+        if (validateFields(txtProductName, txtAmount)) {
+            mainScope.launch {
+                val product = Product(
+                    name = txtProductName.text.toString(),
+                    quantity = txtAmount.text.toString().toInt()
+                )
+
+                withContext(Dispatchers.IO) {
+                    productRepository.insertProduct(product)
+                }
+
+                getShoppingListFromDatabase()
+            }
+        }
+    }
+
+    private fun validateFields(txtProductName: EditText, txtAmount: EditText):Boolean {
+        return if (txtProductName.text.toString().isNotBlank()
+                && txtAmount.text.toString().isNotBlank()) {
+            true
+        } else {
+            Toast.makeText(applicationContext, "Please fill in the fields", Toast.LENGTH_LONG).show()
+            false
+        }
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val productToDelete = products[position]
+                mainScope.launch {
+                    withContext(Dispatchers.IO) {
+                        productRepository.deleteProduct(productToDelete)
+                    }
+                    getShoppingListFromDatabase()
+                }
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
 }
