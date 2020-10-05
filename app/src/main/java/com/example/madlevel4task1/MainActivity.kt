4bc5dd59a8c65f39e3019e32090001418a353f40
Marc Gomersbach <marc.gomersbach@hva.nl>
2020-10-05 15:47:25 +0200

    Updated MainActivity to support the database, atleast partially without the ability to add or remove products just yet.

--- app/src/main/java/com/example/madlevel4task1/MainActivity.kt
+++ app/src/main/java/com/example/madlevel4task1/MainActivity.kt
@@ -6,21 +6,25 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private val products = arrayListOf<Product>()
     private val shoppingListAdapter = ShoppingListAdapter(products)
 
+    private lateinit var productRepository: ProductRepository
+    private val mainScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        productRepository = ProductRepository(applicationContext)
+        getShoppingListFromDatabase()
 
-
-    private fun initViews() {
-        rv_shoppinglist.layoutManager = LinearLayoutManager(applicationContext, RecyclerView.VERTICAL, false)
-        rv_shoppinglist.adapter = shoppingListAdapter
-        rv_shoppinglist.addItemDecoration(DividerItemDecoration(applicationContext, DividerItemDecoration.VERTICAL))
+        initViews()
 
         fab_delete.setOnClickListener {
             onListDelete()
@@ -30,6 +34,31 @@ class MainActivity : AppCompatActivity() {
             onAddProduct()
         }
     }
+
+
+    private fun initViews() {
+        rv_shoppinglist.layoutManager = LinearLayoutManager(applicationContext, RecyclerView.VERTICAL, false)
+        rv_shoppinglist.adapter = shoppingListAdapter
+        rv_shoppinglist.addItemDecoration(DividerItemDecoration(applicationContext, DividerItemDecoration.VERTICAL))
+
+        //createItemTouchHelper
+
+        rv_shoppinglist.apply {
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun getShoppingListFromDatabase() {
+        mainScope.launch {
+            val shoppingList = withContext(Dispatchers.IO) {
+                productRepository.getAllProducts()
+            }
+            this@MainActivity.products.clear()
+            this@MainActivity.products.addAll(shoppingList)
+            this@MainActivity.shoppingListAdapter.notifyDataSetChanged()
+        }
+    }
+
 
 
     private fun onListDelete() {
